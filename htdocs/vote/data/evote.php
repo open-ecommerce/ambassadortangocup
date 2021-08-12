ebf1eee0dc6e5cf6ce338d35ef430b296ea7578a
Eduardo Silva <eduardo.silva@wirelesslogic.com>
2021-08-12 17:53:50 +0100

    mail function

--- htdocs/vote/data/evote.php
+++ htdocs/vote/data/evote.php
@@ -476,7 +476,7 @@ class Evote {
 
         mail($to, $subject, $message, $headers);
 
-        $sql1 = "UPDATE elections_codes SET email=".$email." WHERE code=$personal_code";
+        $sql1 = "UPDATE elections_codes SET email=".$email." WHERE code=".$personal_code;
         $conn->multi_query($sql1);
         echo $conn->error;
         return TRUE;
